d604cbd2c98571209940baccb5c7f4b0e7cf11d4
user1990 <paulius.rimg1990@gmail.com>
2017-10-04 16:11:56 +0300

    # Some changes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 // Styles
-import './App.css';
+import './App.css'
 // React-Router
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
 // Material-ui
-import AppBar from 'material-ui/AppBar';
-import Drawer from 'material-ui/Drawer';
-import MenuItem from 'material-ui/MenuItem';
-import injectTapEventPlugin from 'react-tap-event-plugin';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import fusTheme from './fusTheme';
+import AppBar from 'material-ui/AppBar'
+import Drawer from 'material-ui/Drawer'
+import MenuItem from 'material-ui/MenuItem'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
+import fusTheme from './fusTheme'
 
-injectTapEventPlugin();
+injectTapEventPlugin()
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
+  constructor (props) {
+    super(props)
+    this.state = { open: false }
   }
 
-  render() {
+  render () {
     return (
       <BrowserRouter>
         <MuiThemeProvider muiTheme={getMuiTheme(fusTheme)}>
-          <div className="App">
+          <div className='App'>
             <AppBar
-              title="Title"
-              iconClassNameRight="muidocs-icon-navigation-expand-more"
-              onLeftIconButtonTouchTap={() => this.setState({open: !this.state.open})}
+              title='Title'
+              iconClassNameRight='muidocs-icon-navigation-expand-more'
+              onLeftIconButtonTouchTap={() =>
+                this.setState({ open: !this.state.open })}
             />
             <Drawer
               open={this.state.open}
               docked={false}
-              onRequestChange={(open) => this.setState({open})}
+              onRequestChange={open => this.setState({ open })}
             >
               <MenuItem>
                 <Link to='/'>Home</Link>
@@ -59,21 +60,48 @@ class App extends Component {
             </Drawer>
 
             <Route exact path='/' render={() => <h1>Home View</h1>} />
-            <Route exact path='/logos-posters' render={() => <h1>Logos & Posters View</h1>} />
+            <Route
+              exact
+              path='/logos-posters'
+              render={() => <h1>Logos & Posters View</h1>}
+            />
             <Route exact path='/logs' render={() => <h1>Logos View</h1>} />
             <Route exact path='/posters' render={() => <h1>Posters View</h1>} />
-            <Route exact path='/letterhead' render={() => <h1>Letterhead View</h1>} />
-            <Route exact path='/share-a-story' render={() => <h1>Share a Story View</h1>} />
-            <Route exact path='/planning-guide' render={() => <h1>Planning Guide View</h1>} />
-            <Route exact path='/services' render={() => <h1>Services View</h1>} />
-            <Route exact path='/glossary' render={() => <h1>Glossary View</h1>} />
-            <Route exact path='/service-request-form' render={() => <h1>Service Request Form View</h1>} />
-
+            <Route
+              exact
+              path='/letterhead'
+              render={() => <h1>Letterhead View</h1>}
+            />
+            <Route
+              exact
+              path='/share-a-story'
+              render={() => <h1>Share a Story View</h1>}
+            />
+            <Route
+              exact
+              path='/planning-guide'
+              render={() => <h1>Planning Guide View</h1>}
+            />
+            <Route
+              exact
+              path='/services'
+              render={() => <h1>Services View</h1>}
+            />
+            <Route
+              exact
+              path='/glossary'
+              render={() => <h1>Glossary View</h1>}
+            />
+            <Route
+              exact
+              path='/service-request-form'
+              render={() => <h1>Service Request Form View</h1>}
+            />
           </div>
         </MuiThemeProvider>
       </BrowserRouter>
-    );
+    )
   }
 }
 
-export default App;
+export default App
