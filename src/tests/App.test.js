5b47ca5ab1d4062e298b454077121a63792c8806
user1990 <paulius.rimg1990@gmail.com>
2017-10-05 17:52:52 +0300

    # Create testing files

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,7 @@
+/* eslint-env jest */
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from '../index';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
---
+++ src/tests/Home.test.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+import nightmare from 'nightmare';
+
+const visit = (path) => {
+  return nightmare().goto(`http://localhost:3000${path}`);
+};
+
+describe('Home Page', () => {
+  it('loads on /', async () => {
+    const page = visit('/');
+    const text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).not.toContain('Page not found');
+  });
+
+  it('contains "Resource Center"', async () => {
+    const page = visit('/');
+    const text = await page.evaluate(() => document.body.textContent).end();
+
+    expect(text).toContain('Resource Center');
+  });
+
+  it('shows 6 cards', async () => {
+    const page = visit('/');
+    const selector = '.container .col.s12.m4';
+    const cardCount = await page
+      .wait(selector)
+      .evaluate(sel => document.querySelectorAll(sel).length, selector)
+      .end();
+
+    expect(cardCount).toEqual(9);
+  });
+});
