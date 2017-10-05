8a47e7eaa98fe5bb5699f5a9ff5dd8f916993137
user1990 <paulius.rimg1990@gmail.com>
2017-10-05 17:38:29 +0300

    # Logos page Generic Card v1.1

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
+import { CardTitle } from 'material-ui/Card';
+import { GenericCard } from './../components/GenericCard';
 
 class Logos extends Component {
   render() {
     return (
-      <div>
-        <h1>Logos View</h1>
+      <div className='row'>
+        <div className='col m6'>
+          <GenericCard
+            headerTitle='Hello'
+            headerAvatar='http://via.placeholder.com/140x100'
+            mediaImgSrc='http://via.placeholder.com/120x100'
+            overlay={
+              <CardTitle
+                title='Nice Job'
+                subtitle='Congratulations on this great Achievement'
+              />
+            }
+            cardTitle='Nice one'
+            cardSubtitle='Looking good'
+          >
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam
+            iusto, quis dignissimos fugiat qui architecto. Numquam illum dolor
+            officia qui sit possimus sint quisquam provident, a laudantium
+            reiciendis asperiores perferendis!
+          </GenericCard>
+        </div>
       </div>
     );
   }
