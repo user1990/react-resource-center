19d7e609ae27eb95bd14c2f6f98329210d263c0d
user1990 <paulius.rimg1990@gmail.com>
2017-10-06 09:27:27 +0300

    # Remove hard-coded environment from testUtils.js

--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -1,7 +1,17 @@
 /* eslint-disable */
-import nightmare from 'nightmare';
+import Nightmare from 'nightmare'
+import url from 'url'
+require('nightmare-upload')(Nightmare)
+global['jasmine'].DEFAULT_TIMEOUT_INTERVAL = process.env.TEST_TIMEOUT || 600000
 
-export const visit = (path) => {
+const BASE_URL = url.format({
+  protocol: process.env.PROTOCOL || 'http',
+  hostname: process.env.HOST || 'localhost',
+  port: process.env.PORT || 3000
+})
+
+export const visit = path => {
+  const location = url.resolve(BASE_URL, path)
   const config = {
     // Try changing this to true and run the tests
     // It is pretty cool
@@ -9,7 +19,12 @@ export const visit = (path) => {
     // From the docs: Note that, even though goto normally waits
     // for all the resources on a page to load, a timeout exception
     // is only raised if the DOM itself has not yet loaded.
-    gotoTimeout: 4000,
-  };
-  return nightmare(config).goto(`http://localhost:3000/${path}`);
-};
+    gotoTimeout: process.env.NIGHTMARE_GOTO_TIMEOUT || 300000,
+    waitTimeout: 300000,
+    loadTimeout: 300000,
+    executionTimeout: 300000
+  }
+
+  const nightmare = Nightmare(config)
+  return nightmare.goto(location)
+}
