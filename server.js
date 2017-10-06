d0ca84e8d4873c5dac6c71e315b4d53a66f25df6
user1990 <paulius.rimg1990@gmail.com>
2017-10-06 21:07:11 +0300

    # Test sending emails from server.js via sendgrid

--- server.js
+++ server.js
@@ -2,19 +2,33 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const formidable = require('formidable');
+const helper = require('sendgrid').mail;
 require('dotenv').config();
 
-const PORT = process.env.PORT || 5000;
-const CLIENT_PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 9000;
+// const CLIENT_PORT = process.env.PORT || 3000;
 const PROTOCOL = process.env.PROTOCOL || 'http';
 const HOSTNAME = process.env.HOST || 'localhost';
 const UPLOAD_DIR = path.join(__dirname, 'uploads/');
 const CORS =
-  process.env.NODE_ENV === 'production'
-    ? `${PROTOCOL}://${HOSTNAME}`
-    : `${PROTOCOL}://${HOSTNAME}:${CLIENT_PORT}`;
+  process.env.NODE_ENV === 'production' ? `${PROTOCOL}://${HOSTNAME}` : `*`;
 
-const app = express();
+const fromEmail = new helper.Email('test@example.com');
+const toEmail = new helper.Email('paulius.rimg1990@gmail.com');
+const subject = 'Sending with SendGrid is Fun';
+const content = new helper.Content(
+  'text/plain',
+  'and easy to do anywhere, even with Node.js'
+);
+const mail = new helper.Mail(fromEmail, subject, toEmail, content);
+
+const sg = require('sendgrid')(process.env.SENDGRID_API_KEY);
+const request = sg.emptyRequest({
+  method: 'POST',
+  path: '/v3/mail/send',
+  body: mail.toJSON(),
+});
 
 if (!fs.existsSync(UPLOAD_DIR)) {
   console.warn('==========================');
@@ -35,7 +49,9 @@ app.get('/*', (req, res) => {
 app.post('/uploads', (req, res) => {
   const form = new formidable.IncomingForm();
 
-  form.parse(req);
+  form.parse(req).on('field', (field, value) => {
+    console.log(field, value)
+  });
 
   form.on('fileBegin', (name, file) => {
     file.path = `${UPLOAD_DIR}${file.name}`;
@@ -53,6 +69,15 @@ app.post('/uploads', (req, res) => {
   res.status(200).json({
     success: true,
     status: 'Form successfully submitted',
+  });
+
+  sg.API(request, (err, res) => {
+    if (err) {
+      console.log('Error response received');
+    }
+    console.log(res.statusCode);
+    console.log(res.body);
+    console.log(res.headers);
   });
 });
 
