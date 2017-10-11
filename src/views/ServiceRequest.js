1c9d3f8c841452a66a6e6b3880795437bfc201ef
user1990 <paulius.rimg1990@gmail.com>
2017-10-11 14:23:26 +0300

    Formsy Form client side validation

--- src/views/ServiceRequest.js
+++ src/views/ServiceRequest.js
@@ -1,55 +1,100 @@
-import React, { Component } from 'react';
-import TextField from 'material-ui/TextField';
-import DatePicker from 'material-ui/DatePicker';
-import Checkbox from 'material-ui/Checkbox';
-import Dialog from 'material-ui/Dialog';
-import FlatButton from 'material-ui/FlatButton';
-import { Link } from 'react-router-dom';
-import RaisedButton from '../components/MaterializeRaisedButton';
-import '../styles/inputFile.css';
-import '../styles/serviceRequest.css';
-import { Helmet } from 'react-helmet';
+// eslint-disable-next-line
+/* global notifyFormError */
+import React, { Component } from 'react'
+import TextField from 'material-ui/TextField'
+import DatePicker from 'material-ui/DatePicker'
+import Checkbox from 'material-ui/Checkbox'
+import Dialog from 'material-ui/Dialog'
+import FlatButton from 'material-ui/FlatButton'
+import { Link } from 'react-router-dom'
+import RaisedButton from '../components/MaterializeRaisedButton'
+import '../styles/inputFile.css'
+import '../styles/serviceRequest.css'
+import { Helmet } from 'react-helmet'
+import Formsy from 'formsy-react'
+import { FormsyText, FormsyCheckbox } from 'formsy-material-ui/lib'
 
-const PORT = process.env.SERVER_PORT || 9000;
-const HOST = process.env.UPLOADS_HOST || window.location.host.split(':')[0];
-const UPLOAD_URL = `http://${HOST}:${PORT}/uploads`;
+const PORT = process.env.SERVER_PORT || 9000
+const HOST = process.env.UPLOADS_HOST || window.location.host.split(':')[0]
+const UPLOAD_URL = `http://${HOST}:${PORT}/uploads`
 
 const styles = {
-  block: {
-    maxWidth: 250
-  },
   checkbox: {
     marginBottom: 16
-  },
-  button: {
-    margin: 12
-  },
-  exampleImageInput: {
-    cursor: 'pointer',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    width: '100%',
-    opacity: 0
   }
-};
+}
+
+const singleLineFields = [
+  { name: 'Name', required: true, type: 'isAlpha', error: 'Not a valid name' },
+  {
+    name: 'Email',
+    required: true,
+    type: 'isEmail',
+    error: 'Not a valid email'
+  },
+  {
+    name: 'Phone',
+    required: true,
+    type: 'isNumeric',
+    error: 'Not a valid phone number'
+  },
+  { name: 'Department', required: false }
+]
+
+const multiLineFields = [
+  {
+    name: 'Project Description',
+    required: true,
+    type: 'isAlphanumeric',
+    error: 'Not a valid project description'
+  },
+  {
+    name: 'Project Goal',
+    required: true,
+    type: 'isAlphanumeric',
+    error: 'Not a valid goal'
+  },
+  {
+    name: 'Project Budget',
+    required: true,
+    type: 'isNumeric',
+    error: 'Not a valid number'
+  },
+  {
+    name: 'Key Messages',
+    required: true,
+    type: 'isAlphanumeric',
+    error: 'Not a valid key message'
+  },
+  {
+    name: 'Primary Target Audience',
+    required: true,
+    type: 'isAlphanumeric',
+    error: 'Not a valid target'
+  },
+  {
+    name: 'Secondary Target Audience',
+    required: true,
+    type: 'isAlphanumeric',
+    error: 'Not a valid target'
+  },
+  {
+    name: 'Project Contact (if other than yourself)',
+    required: false,
+    type: null,
+    error: null
+  },
+  {
+    name: 'Comments',
+    required: false,
+    type: 'isAlphanumeric',
+    error: 'Not a valid comment'
+  }
+]
 
 class ServiceRequest extends Component {
-  constructor(props) {
-    super(props);
-    this.singleLineFields = ['Name', 'Email', 'Phone', 'Department'];
-    this.multiLineFields = [
-      'Project Description',
-      'Project Goal',
-      'Project Budget',
-      'Key Messages',
-      'Primary Target Audience',
-      'Secondary Target Audience',
-      'Project Contact (if other than yourself)',
-      'Comments'
-    ];
+  constructor (props) {
+    super(props)
     this.leftCheckboxes = [
       'Simple1',
       'Simple2',
@@ -57,7 +102,7 @@ class ServiceRequest extends Component {
       'Simple4',
       'Simple5',
       'Simple6'
-    ];
+    ]
     this.rightCheckboxes = [
       'Simple7',
       'Simple8',
@@ -65,17 +110,7 @@ class ServiceRequest extends Component {
       'Simple10',
       'Simple11',
       'Simple12'
-    ];
-    const stringProps = [
-      ...this.singleLineFields,
-      ...this.multiLineFields
-    ].reduce(
-      (acc, label) => ({
-        [this.formatLabelToProperty(label)]: '',
-        ...acc
-      }),
-      {}
-    );
+    ]
     const checkboxProps = [
       ...this.leftCheckboxes,
       ...this.rightCheckboxes
@@ -85,7 +120,7 @@ class ServiceRequest extends Component {
         ...acc
       }),
       {}
-    );
+    )
     this.state = {
       form: {
         fileInput: null
@@ -94,115 +129,99 @@ class ServiceRequest extends Component {
       resultDialogOpen: false,
       resultdialogText: null,
       resultdialogSuccess: true
-    };
-    Object.assign(this.state.form, stringProps, checkboxProps);
+    }
+    Object.assign(this.state.form, checkboxProps)
+    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  formatLabelToProperty = label => {
+  notifyFormError = () => {
+    this.setState({
+      resultDialogOpen: true,
+      resultdialogSuccess: false,
+      resultdialogText: 'Something went wrong. Check for errors and try again.'
+    })
+  };
+
+  formatLabelToProperty = label =>
     label
       .split(' (')[0]
       .toLowerCase()
       .split(' ')
       .join('-');
-  };
 
-  handleInputChange = event => {
-    const target = event.target;
-    const value = target.type === 'checkbox' ? target.checked : target.value;
-    const name = target.name;
+  handleInputChange (event) {
+    const target = event.target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+    const name = target.name
 
-    this.setState({
-      [name]: value
-    });
-  };
+    const form = Object.assign({}, this.state.form)
+    form[name] = value
+
+    this.setState({ form })
+  }
 
   handleFilePath = event => {
-    const target = event.target;
-    const files = Array.from(target.files);
-    let fileNames = null;
+    const target = event.target
+    const files = Array.from(target.files)
+    let fileNames = null
     if (files.length > 0) {
-      fileNames = files.map(f => f.name).join(', ');
+      fileNames = files.map(f => f.name).join(', ')
     }
 
-    const form = Object.assign({}, this.state.form);
-    form.fileInput = fileNames;
-    this.setState({ form });
+    const form = Object.assign({}, this.state.form)
+    form.fileInput = fileNames
+    this.setState({ form })
   };
 
   handleFormData = async () => {
-    const data = new FormData();
-    // eslint-disable-next-line
+    const data = new FormData()
     for (const [key, val] of Object.entries(this.state.form)) {
-      data.append(key, val);
+      data.append(key, val)
     }
-    // eslint-disable-next-line
-    for (const file of this.uploadInput.files) data.append('file', file);
 
-    this.setState({ loadingDialogOpen: true });
+    for (const file of this.uploadInput.files) data.append('file', file)
+
+    this.setState({ loadingDialogOpen: true })
 
     try {
       const response = await fetch(UPLOAD_URL, {
         method: 'post',
         body: data
-      }).then(res => res.json());
+      }).then(res => res.json())
 
       if (!response.success) {
-        throw response.status;
+        throw response.status
       }
 
       this.setState({
         resultDialogOpen: true,
         resultdialogSuccess: true,
         resultdialogText: 'Your service request was sent successfully.'
-      });
+      })
     } catch (err) {
       const msg =
         typeof err === 'string'
           ? err
-          : 'An error accured while sending the request.';
+          : 'An error accured while sending the request.'
       this.setState({
         resultDialogOpen: true,
         resultdialogSuccess: false,
         resultdialogText: msg
-      });
+      })
     }
 
-    this.setState({ loadingDialogOpen: false });
+    this.setState({ loadingDialogOpen: false })
   };
 
   handleDialogClose = () => {
-    this.setState({ resultDialogOpen: false });
+    this.setState({ resultDialogOpen: false })
   };
 
-  render() {
-    const fileValue = this.state.form.fileInput || 'Select a file to upload';
+  render () {
+    const fileValue = this.state.form.fileInput || 'Select a file to upload'
 
-    const SingleLineField = (label, index) => (
-      <div className="col s12 m6" key={index}>
-        <TextField
-          floatingLabelText={label}
-          name={this.formatLabelToProperty(label)}
-          value={this.state.form[this.formatLabelToProperty(label)]}
-          onChange={this.handleInputChange}
-          fullWidth
-        />
-      </div>
-    );
-    const MultiLineField = (label, index) => (
-      <div className="col s12 m6" key={index}>
-        <TextField
-          floatingLabelText={label}
-          name={this.formatLabelToProperty(label)}
-          value={this.state[this.formatLabelToProperty(label)]}
-          onChange={this.handleInputChange}
-          multiLine
-          rows={2}
-          fullWidth
-        />
-      </div>
-    );
     const CheckboxField = (label, index) => (
-      <div className="col s12 m6" key={index}>
+      <div className='col s12 m6' key={index}>
         <Checkbox
           label={label}
           name={this.formatLabelToProperty(label)}
@@ -212,112 +231,146 @@ class ServiceRequest extends Component {
           style={styles.checkbox}
         />
       </div>
-    );
+    )
     return (
-      <div className="container">
+      <div className='container'>
         <Helmet>
           <title>Service Request | Resource Center</title>
         </Helmet>
-        <div className="row">
-          <div className="col s12 flow-text">
+        <div className='row'>
+          <div className='col s12 flow-text'>
             <h2>Please use this form to request services.</h2>
           </div>
         </div>
-        <div className="row">
-          {this.singleLineFields.map((label, index) =>
-            SingleLineField(label, index)
-          )}
-          {this.multiLineFields.map((label, index) =>
-            MultiLineField(label, index)
-          )}
-          <div className="col s12 m6">
-            <DatePicker hintText="Desired Completion Date" />
-          </div>
-          <div className="col s12 m6">
-            <div className="file-field input-field">
-              <div className="btn">
-                <span>Upload Files</span>
-                <input
-                  id="upload"
-                  name="upload[]"
-                  type="file"
-                  multiple
-                  onChange={this.handleFilePath}
-                  ref={input => {
-                    this.uploadInput = input;
-                  }}
-                />
-              </div>
-              <div className="file-path-wrapper">
-                <TextField
-                  className="file-path validate"
-                  value={fileValue}
-                  multiline
-                  rows={1}
-                  fullWidth
-                  readOnly
-                />
-              </div>
-            </div>
-          </div>
-          <div className="col s12 m6">
-            {this.leftCheckboxes.map((label, index) => {
-              return CheckboxField(label, index);
-            })}
-          </div>
-          <div className="col s12 m6">
-            {this.rightCheckboxes.map((label, index) => {
-              return CheckboxField(label, index);
-            })}
-          </div>
-          <div className="col s12">
-            <RaisedButton
-              label="Submit"
-              id="submit-button"
-              onClick={this.handleFormData}
-              primary
-            />
-            <div id="planning-guide-checkbox">
-              <Checkbox
-                label={
-                  <span>
-                    I have read the{' '}
-                    <Link
-                      to="/planning-guide"
-                      target="_blank"
-                      style={{ fontWeight: 500 }}
-                    >
-                      Planning Guide
-                    </Link>
-                  </span>
-                }
-                style={styles.checkbox}
-                inputStyle={{ width: '35px' }}
-              />
-            </div>
-          </div>
-          <Dialog title="Loading..." modal open={this.state.loadingDialogOpen}>
-            Sending service request
-          </Dialog>
-          <Dialog
-            title={this.state.resultDialogSuccess ? 'Done' : 'Error'}
-            modal={false}
-            open={this.state.resultDialogOpen}
-            onRequestClose={this.handleDialogClose}
-            actions={[
-              <FlatButton
-                label="Ok"
-                onTouchTap={this.handleDialogClose}
-                keyboardFocused
-              />
-            ]}
+        <div className='row'>
+          <Formsy.Form
+            onValid={this.enableButton}
+            onInvalid={this.disableButton}
+            onValidSubmit={this.handleFormData}
+            onInvalidSubmit={this.notifyFormError}
           >
-            this.state.resultdialogText
-          </Dialog>
+            {singleLineFields.map((field, index) => (
+              <div className='col s12 m6' key={index}>
+                <FormsyText
+                  floatingLabelText={field.name}
+                  name={field.name.toLowerCase()}
+                  value={this.state.form[field.name]}
+                  onChange={this.handleInputChange}
+                  fullWidth
+                  id={`${field.name.toLowerCase()}-field`}
+                  required={field.required}
+                  validations={field.type}
+                  validationeError={field.error}
+                />
+              </div>
+            ))}
+            {multiLineFields.map((field, index) => (
+              <div className='col s12 m6' key={index}>
+                <FormsyText
+                  floatingLabelText={field.name}
+                  name={field.name.toLowerCase()}
+                  value={this.state.form[field.name]}
+                  onChange={this.handleInputChange}
+                  fullWidth
+                  id={`${field.name.toLowerCase()}-field`}
+                  required={field.required}
+                  validations={field.type}
+                  validationeError={field.error}
+                />
+              </div>
+            ))}
+            <div className='col s12 m6'>
+              <DatePicker hintText='Desired Completion Date' />
+            </div>
+            <div className='col s12 m6'>
+              <div className='file-field input-field'>
+                <div className='btn'>
+                  <span>Upload Files</span>
+                  <input
+                    id='upload'
+                    name='upload[]'
+                    type='file'
+                    multiple
+                    onChange={this.handleFilePath}
+                    ref={input => {
+                      this.uploadInput = input
+                    }}
+                  />
+                </div>
+                <div className='file-path-wrapper'>
+                  <TextField
+                    className='file-path validate'
+                    value={fileValue}
+                    multiline
+                    rows={1}
+                    fullWidth
+                    readOnly
+                    id='fiel-path-field'
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='col s12 m6'>
+              {this.leftCheckboxes.map((label, index) => {
+                return CheckboxField(label, index)
+              })}
+            </div>
+            <div className='col s12 m6'>
+              {this.rightCheckboxes.map((label, index) => {
+                return CheckboxField(label, index)
+              })}
+            </div>
+            <div className='col s12'>
+              <RaisedButton
+                label='Submit'
+                id='submit-button'
+                type='submit'
+                primary
+              />
+              <div id='planning-guide-checkbox'>
+                <FormsyCheckbox
+                  name='planning-guide-check'
+                  required
+                  label={
+                    <span>
+                      I have read the{' '}
+                      <Link
+                        to='/planning-guide'
+                        target='_blank'
+                        style={{ fontWeight: 500 }}
+                      >
+                        Planning Guide
+                      </Link>
+                    </span>
+                  }
+                  style={styles.checkbox}
+                  inputStyle={{ width: '35px' }}
+                />
+              </div>
+            </div>
+          </Formsy.Form>
         </div>
+        <Dialog title='Loading...' modal open={this.state.loadingDialogOpen}>
+          Sending service request
+        </Dialog>
+        <Dialog
+          title={this.state.resultDialogSuccess ? 'Done' : 'Error'}
+          modal={false}
+          open={this.state.resultDialogOpen}
+          onRequestClose={this.handleDialogClose}
+          actions={[
+            <FlatButton
+              label='Ok'
+              onTouchTap={this.handleDialogClose}
+              keyboardFocused
+            />
+          ]}
+        >
+          this.state.resultdialogText
+        </Dialog>
       </div>
-    );
+    )
   }
 }
 
-export default ServiceRequest;
+export default ServiceRequest
