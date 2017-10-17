c2df22cb8767f797f6d567b472f10fc0f76785ac
user1990 <paulius.rimg1990@gmail.com>
2017-10-17 10:29:16 +0300

    # Updated logos view

--- src/views/Logos.js
+++ src/views/Logos.js
@@ -2,27 +2,24 @@ import map from 'lodash/map'
 import React, { Component } from 'react'
 import SelectField from 'material-ui/SelectField'
 import MenuItem from 'material-ui/MenuItem'
-import MasonryComp from '../components/MasonryComp'
 import { Helmet } from 'react-helmet'
 import { logos, tabs } from '../data/logoData.js'
+import MasonryComp from '../components/MasonryComp'
 import '../styles/logos.css'
-import { logPageView } from '../utils/analytics'
+import { Tabs, Tab } from 'material-ui/Tabs'
 
 class Logos extends Component {
   state = {
-    activeTab: 'all',
+    activeTab: 'franciscan',
     data: logos,
     type: 'all'
   }
 
-  componentDidMount() {
-    logPageView()
-  }
-
-  handleChange = (event, index, value) =>
+  handleTabChange = value => {
     this.setState({
       activeTab: value
-    });
+    })
+  }
 
   render () {
     const { activeTab } = this.state
@@ -32,23 +29,18 @@ class Logos extends Component {
         <Helmet>
           <title>Logos | Resource Center</title>
         </Helmet>
+
+        <Tabs
+          value={activeTab}
+          onChange={this.handleTabChange}
+          className='hide-on-med-and-down'
+        >
+          {map(tabs, (tab, tabKey) => (
+            <Tab label={tabs[tabKey]} value={tabKey} key={tabKey + 'tab'} />
+          ))}
+        </Tabs>
+
         <div className='row'>
-          <div className='col s12 hide-on-med-and-down'>
-            <ul className='tabs'>
-              {map(tabs, (tab, tabKey) => (
-                <li className='tab' key={`${tabKey}'li'`}>
-                  <a
-                    key={tabKey}
-                    href={`'#'${tabKey}`}
-                    className={tabKey === activeTab && 'active'}
-                    onClick={() => this.setState({ activeTab: tabKey })}
-                  >
-                    {tabs[tabKey]}
-                  </a>
-                </li>
-              ))}
-            </ul>
-          </div>
           <div
             className='col s12 hide-on-large-only'
             style={{ textAlign: 'center' }}
@@ -56,25 +48,30 @@ class Logos extends Component {
             <SelectField
               floatingLabelText='Logo Type'
               value={this.state.activeTab}
-              onChange={this.handleChange}
+              onChange={(event, index, value) => this.handleTabChange(value)}
               style={{ textAlign: 'left', width: '100%' }}
             >
               {map(tabs, (tab, tabKey) => (
-                <MenuItem value={tabKey} primaryText={tabs[tabKey]} />
+                <MenuItem
+                  value={tabKey}
+                  primaryText={tabs[tabKey]}
+                  key={tabKey}
+                />
               ))}
             </SelectField>
           </div>
         </div>
+
         {/* For each tab, we generate a row */}
         {map(tabs, (tab, tabKey) => (
-          <div className='row' id={tabKey} key={tabKey}>
+          <div className='row' style={{ marginBottom: 0 }} key={tabKey}>
             {/* We render masonry comp only if we are in current active tab key */}
-            {activeTab === tabKey &&
+            {activeTab === tabKey && (
               <MasonryComp
                 data={this.state.data}
                 activeTab={this.state.activeTab}
               />
-            }
+            )}
           </div>
         ))}
       </div>
